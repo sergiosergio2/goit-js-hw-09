f8c7d8c083a623d179305b09f1a59e9718d99c91
sergiosergio2 <vzdor5555@gmail.com>
2021-11-28 22:34:05 +0200

    Task 1 finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+ref = {
+startBtn: document.querySelector('button[data-start]'),
+stopBtn: document.querySelector('button[data-stop]'),
+body: document.querySelector('body'),
+}
 
+ref.startBtn.addEventListener('click', start);
+ref.stopBtn.addEventListener('click', stop);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function start() {
+      bodyColor();
+      timerId = setInterval(bodyColor, 1000);
+      isabled(ref.startBtn, ref.stopBtn);
+  }
+  function stop() {
+      isabled(ref.stopBtn, ref.startBtn );
+      clearInterval(timerId);
+  }
+
+  function bodyColor() {
+      ref.body.setAttribute('style', `background-color: ${getRandomHexColor()};`);
+
+
+  }
+  function isabled (activ, pass) {
+      activ.setAttribute('disabled','');
+      pass.removeAttribute('disabled');
+  }
