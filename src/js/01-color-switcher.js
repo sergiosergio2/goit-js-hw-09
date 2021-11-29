590d68f83c0aaf49abd04ce6157060d3888ef135
sergiosergio2 <vzdor5555@gmail.com>
2021-11-29 15:49:48 +0200

    added css

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,11 @@
-ref = {
+const refs = {
 startBtn: document.querySelector('button[data-start]'),
 stopBtn: document.querySelector('button[data-stop]'),
 body: document.querySelector('body'),
 }
 
-ref.startBtn.addEventListener('click', start);
-ref.stopBtn.addEventListener('click', stop);
+refs.startBtn.addEventListener('click', start);
+refs.stopBtn.addEventListener('click', stop);
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -14,15 +14,15 @@ function getRandomHexColor() {
   function start() {
       bodyColor();
       timerId = setInterval(bodyColor, 1000);
-      disabled(ref.startBtn, ref.stopBtn);
+      disabled(refs.startBtn, refs.stopBtn);
   }
   function stop() {
-      disabled(ref.stopBtn, ref.startBtn );
+      disabled(refs.stopBtn, refs.startBtn );
       clearInterval(timerId);
   }
 
   function bodyColor() {
-      ref.body.setAttribute('style', `background-color: ${getRandomHexColor()};`);
+      refs.body.setAttribute('style', `background-color: ${getRandomHexColor()};`);
 
 
   }
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,20 +1,42 @@
 import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-ref {
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds'), 
 
 }
+let selectedTime = null;
+let timerId;
+refs.btnStart.addEventListener ('click', startTimer);
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
-  flatpickr("#datetime-picker", options) 
+        selectedTime = selectedDates[0].getTime();
+        const  timeResult = selectedTime - new Date().getTime();
+    
+        if(timeResult < 0) {
+            setDisabled(refs.btnStart);
+            Notify.failure ('Plase choose a date in the future');
+        } else {
+            removeDisabled(refs.btnStart);
+        }
+  }
+}
+function setDisabled (element) {
+if (element.hasAttribute('disabled')) {
+    return;
+}
+element.removeAttribute('disabled');
+}
+  flatpickr("#datetime-picker", options); 
 
   function convertMs(ms) {
     // Number of milliseconds per unit of time
@@ -48,5 +70,41 @@ const options = {
             }
             return padString.slice(0,targetLength) + String(this);
         }
-    };
+    }
   }
+  function startTimer() {
+      setDisabled(refs.btnStart);
+      if (timerId) {
+          clearInterval(timerId);
+      }
+      addTime(selectedTime);
+      timerId = setInterval(addTime, 1000, selectedTime);
+      
+  }
+  const addLeadingZero = (value) => {
+    return value.toString().padStart(2,"0");
+    }
+    function clearTime () {
+        clearInterval(timerId);
+        refs.days.textContent = '00';
+        refs.hours.textContent = '00';
+        refs.minutes.textContent = '00';
+        refs.seconds.textContent = '00';
+    }
+    const addTime = (timeMs) => {
+        const  timeResult = timeMs-new Date().getTime();
+        if(timeResult<0){
+            setDisabled(refs.btnStart);
+            clearTime();
+            Notify.success('Bo-o-om');   
+             return;
+         } 
+       const time =convertMs(timeResult);
+       refs.days.textContent=addLeadingZero(time.days);
+       refs.hours.textContent=addLeadingZero(time.hours);
+    refs.minutes.textContent=addLeadingZero(time.minutes);
+    refs.seconds.textContent=addLeadingZero(time.seconds);
+     }
+     setDisabled (refs.btnStart);
+
+
